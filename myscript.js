7594d359c88cbdef4d30e8c46bdb04fb5573c422
Sai Charan <saicharan07071979@gmail.com>
2024-05-01 16:12:15 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,23 @@
+﻿const elmColors = document.getElementsByName("color");
+const colors = ["red", "blue"];
+
+for (let i = 0; i < elmColors.length; i++) {
+  elmColors[i].onclick = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: setBackGroundColor,
+        args: [colors[i]]
+      });
+    });
+  }
+}
+
+function setBackGroundColor(color) {
+  console.log("setBackGroundColor color=", color);
+  document.body.style.backgroundColor = color;
+  const input = document.querySelector('.gLFyf')
+  input.value="Spark Automation";
+  const btn = document.querySelector(".gNO89b")
+  btn.click();
+}
